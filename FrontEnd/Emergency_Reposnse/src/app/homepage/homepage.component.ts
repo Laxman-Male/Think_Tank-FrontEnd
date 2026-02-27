551c07e8a46fd0419740b45c787e78089754b498
Laxman-Male <laxmanmale122@gmail.com>
2026-02-27 23:06:22 +0530

    platform feature

--- FrontEnd/Emergency_Reposnse/src/app/homepage/homepage.component.ts
+++ FrontEnd/Emergency_Reposnse/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -10,4 +11,12 @@ export class HomepageComponent {
   //  scrollToSection(id){
   //       document.getElementById(id).scrollIntoView({behavior:"smooth"});
   //   }
+  constructor(private route:Router){}
+  go_to_profile(){
+    this.route.navigate(['/profile'])
+
+  }
+  go_to_feature(){
+    this.route.navigate(['/feature'])
+  }
 }
