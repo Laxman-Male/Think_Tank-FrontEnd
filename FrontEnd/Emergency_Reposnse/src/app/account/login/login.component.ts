a56b2f7fd71e60e45f5f7654bc829f4c9ce53a68
Laxman-Male <laxmanmale122@gmail.com>
2026-02-27 14:59:14 +0530

    login / registration implementation

--- FrontEnd/Emergency_Reposnse/src/app/account/login/login.component.ts
+++ FrontEnd/Emergency_Reposnse/src/app/account/login/login.component.ts
@@ -1,16 +1,73 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private authService: AuthService, private router: Router) {}
+  
+
   userType: 'User' | 'Hospital' = 'User'; // Default selection
+
+  loginData = {
+    email: '',
+    password: '',
+    role: 'User',
+    isNewUser: false
+  };
 
   setUserType(type: 'User' | 'Hospital') {
     this.userType = type;
   }
+
+ onSubmit() {
+  if(this.loginData.isNewUser==true){
+   this.authService.register(this.loginData).subscribe({
+      next: (response) => {
+        // Save the JWT sent by your Go backend
+        localStorage.setItem('token', response.token);
+        
+        // Redirect based on the role to solve "real-time coordination"
+        if (this.userType === 'Hospital') {
+          this.router.navigate(['/hospital-dashboard']);
+        } else {
+          this.router.navigate(['/sos-dashboard']);
+        }
+      },
+      error: (err) => {
+        console.error('Login Error:', err);
+        alert('Invalid credentials. Check your Go server!');
+      }
+    });
+  } else{
+   this.authService.login(this.loginData).subscribe({
+      next: (response) => {
+        // Save the JWT sent by your Go backend
+        localStorage.setItem('token', response.token);
+        
+        // Redirect based on the role to solve "real-time coordination"
+        if (this.userType === 'Hospital') {
+          this.router.navigate(['/hospital-dashboard']);
+        } else {
+          this.router.navigate(['/sos-dashboard']);
+        }
+      },
+      error: (err) => {
+        console.error('Login Error:', err);
+        alert('Invalid credentials. Check your Go server!');
+      }
+    });
+  }
+  console.log(this.loginData.isNewUser)
+ 
+  }
+
 }
